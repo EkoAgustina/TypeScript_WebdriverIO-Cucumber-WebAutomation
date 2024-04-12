ab1f4f57b679ca25760f113aeacb54f8d2d2d611
EkoAgustina <eko.agustina86@gmail.com>
2024-04-12 11:06:12 +0700

    Update filename screenshot

--- hooks/driverHooks.ts
+++ hooks/driverHooks.ts
@@ -85,8 +85,10 @@ async function hooksAfterScenario(world: any, result: any): Promise<void> {
     properties.set('Host', allureHostUrl() || 'Unknown');
     properties.save(propertiesPath);
 
+    console.log(world)
+
     if (result.error) {
-      await takeScreenshot(`failed_${world.result.exception.message}`)
+      await takeScreenshot(`failed_${world.pickle.name}`)
         // cucumberJson.attach(await browser.takeScreenshot(), 'image/png');
     }
 }
