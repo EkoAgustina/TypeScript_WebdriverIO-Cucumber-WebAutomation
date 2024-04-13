5ebaa2849191ba908d6a8700c5d56fda5b6315bf
EkoAgustina <eko.agustina86@gmail.com>
2024-04-13 09:10:13 +0700

    Updated ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,43 +1,17 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended"
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: "module",
+      project: "./tsconfig.json" // path to your tsconfig.json
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "standard"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "no-console": 0,
-        "@typescript-eslint/naming-convention": [
-            "error",
-            {
-              "selector": "variable",
-              "format": ["camelCase", "UPPER_CASE"]
-            }
-          ]
+    rules: {
+      // tambahkan aturan tambahan sesuai kebutuhan Anda
     }
-}
+  };
