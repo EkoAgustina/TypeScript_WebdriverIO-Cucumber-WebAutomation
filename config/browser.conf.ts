931f8d378d4918e40a07789f2e8b1e50dc0829f1
EkoAgustina <eko.agustina86@gmail.com>
2025-01-26 21:29:36 +0700

    Update steps

--- config/browser.conf.ts
+++ config/browser.conf.ts
@@ -21,7 +21,7 @@ if (globalVariables.os === 'linux') {
                             '--disable-extensions',
                             '--disable-dev-shm-usage',
                             '--disable-cache', 
-                            `--user-agent=${globalVariables.getRandomUserAgent()}`]
+                            `--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36`]
             },
             acceptInsecureCerts: true,
             // webSocketUrl: true
